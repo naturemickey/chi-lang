da20ad71166a1630f5d777289ec221d2ab391669
Michael Chow <mickey_zhouyou@yeah.net>
2016-06-05 20:00:14 +0800

    添加几个指令

---
+++ src/chi/vm/cpu/mod.rs
@@ -0,0 +1,11 @@
+
+const ADD : i64 = 1;
+const SUB : i64 = 2;
+const MUT : i64 = 3;
+const DIV : i64 = 4;
+
+const STORE : i64 = 5;
+
+const JUMP_IF_IS_ZERO : i64 = 6;
+const JUMP_IF_LESS_THEN_ZERO : i64 = 7;
+const JUMP_IF_LESS_OR_EQ_ZERO : i64 = 8;
